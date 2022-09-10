2a92c8b2269daf24291170c24e2f911c6f093e89
Armin Pretorius <arminp1139@gmail.com>
2022-09-10 16:58:14 +0200

    Added Forms
    +FrmInventory
    +FrmOrder
    +FrmReporting

---
+++ POS_Group5_CMPG223/POS_Group5_CMPG223/FrmInventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_Group5_CMPG223
+{
+    public partial class FrmInventory : Form
+    {
+        public FrmInventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
