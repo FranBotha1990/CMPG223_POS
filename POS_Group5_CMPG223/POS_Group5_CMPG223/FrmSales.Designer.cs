8aa910e075c976b940499c78b05bc64f2c777ef1
Armin Pretorius <arminp1139@gmail.com>
2022-09-12 05:14:10 +0200

    Added UI and Code to FrmSales
    Do not change code in FrmSales, work in progress

--- POS_Group5_CMPG223/POS_Group5_CMPG223/FrmSales.Designer.cs
+++ POS_Group5_CMPG223/POS_Group5_CMPG223/FrmSales.Designer.cs
@@ -29,12 +29,140 @@ namespace POS_Group5_CMPG223
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pnlBill = new System.Windows.Forms.Panel();
+            this.pnlBillButtons = new System.Windows.Forms.Panel();
+            this.lbxBill = new System.Windows.Forms.ListBox();
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.pnlDiscount = new System.Windows.Forms.Panel();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnPay = new System.Windows.Forms.Button();
+            this.btnDiscItem = new System.Windows.Forms.Button();
+            this.btnDiscBill = new System.Windows.Forms.Button();
+            this.pnlBill.SuspendLayout();
+            this.pnlBillButtons.SuspendLayout();
+            this.pnlDiscount.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBill
+            // 
+            this.pnlBill.Controls.Add(this.lbxBill);
+            this.pnlBill.Controls.Add(this.pnlBillButtons);
+            this.pnlBill.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlBill.Location = new System.Drawing.Point(550, 0);
+            this.pnlBill.Name = "pnlBill";
+            this.pnlBill.Size = new System.Drawing.Size(250, 450);
+            this.pnlBill.TabIndex = 0;
+            // 
+            // pnlBillButtons
+            // 
+            this.pnlBillButtons.Controls.Add(this.btnPay);
+            this.pnlBillButtons.Controls.Add(this.btnDelete);
+            this.pnlBillButtons.Controls.Add(this.pnlDiscount);
+            this.pnlBillButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlBillButtons.Location = new System.Drawing.Point(0, 350);
+            this.pnlBillButtons.Name = "pnlBillButtons";
+            this.pnlBillButtons.Size = new System.Drawing.Size(250, 100);
+            this.pnlBillButtons.TabIndex = 0;
+            // 
+            // lbxBill
+            // 
+            this.lbxBill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxBill.FormattingEnabled = true;
+            this.lbxBill.Location = new System.Drawing.Point(0, 0);
+            this.lbxBill.Name = "lbxBill";
+            this.lbxBill.Size = new System.Drawing.Size(250, 350);
+            this.lbxBill.TabIndex = 1;
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.AutoScroll = true;
+            this.pnlButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlButtons.Location = new System.Drawing.Point(0, 0);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(550, 450);
+            this.pnlButtons.TabIndex = 1;
+            this.pnlButtons.SizeChanged += new System.EventHandler(this.pnlButtons_SizeChanged);
+            // 
+            // pnlDiscount
+            // 
+            this.pnlDiscount.Controls.Add(this.btnDiscBill);
+            this.pnlDiscount.Controls.Add(this.btnDiscItem);
+            this.pnlDiscount.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlDiscount.Location = new System.Drawing.Point(125, 0);
+            this.pnlDiscount.Name = "pnlDiscount";
+            this.pnlDiscount.Size = new System.Drawing.Size(125, 100);
+            this.pnlDiscount.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Location = new System.Drawing.Point(0, 0);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(125, 50);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnPay
+            // 
+            this.btnPay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPay.Location = new System.Drawing.Point(0, 50);
+            this.btnPay.Name = "btnPay";
+            this.btnPay.Size = new System.Drawing.Size(125, 50);
+            this.btnPay.TabIndex = 2;
+            this.btnPay.Text = "Pay";
+            this.btnPay.UseVisualStyleBackColor = true;
+            // 
+            // btnDiscItem
+            // 
+            this.btnDiscItem.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDiscItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiscItem.Location = new System.Drawing.Point(0, 0);
+            this.btnDiscItem.Name = "btnDiscItem";
+            this.btnDiscItem.Size = new System.Drawing.Size(125, 50);
+            this.btnDiscItem.TabIndex = 2;
+            this.btnDiscItem.Text = "Discount Item";
+            this.btnDiscItem.UseVisualStyleBackColor = true;
+            // 
+            // btnDiscBill
+            // 
+            this.btnDiscBill.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnDiscBill.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDiscBill.Location = new System.Drawing.Point(0, 50);
+            this.btnDiscBill.Name = "btnDiscBill";
+            this.btnDiscBill.Size = new System.Drawing.Size(125, 50);
+            this.btnDiscBill.TabIndex = 3;
+            this.btnDiscBill.Text = "Discount Bill";
+            this.btnDiscBill.UseVisualStyleBackColor = true;
+            // 
+            // FrmSales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlButtons);
+            this.Controls.Add(this.pnlBill);
+            this.Name = "FrmSales";
             this.Text = "FrmSales";
+            this.pnlBill.ResumeLayout(false);
+            this.pnlBillButtons.ResumeLayout(false);
+            this.pnlDiscount.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlBill;
+        private System.Windows.Forms.Panel pnlBillButtons;
+        private System.Windows.Forms.ListBox lbxBill;
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.Button btnPay;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Panel pnlDiscount;
+        private System.Windows.Forms.Button btnDiscBill;
+        private System.Windows.Forms.Button btnDiscItem;
     }
 }
--- POS_Group5_CMPG223/POS_Group5_CMPG223/FrmSales.cs
+++ POS_Group5_CMPG223/POS_Group5_CMPG223/FrmSales.cs
@@ -12,14 +12,87 @@ namespace POS_Group5_CMPG223
 {
     public partial class FrmSales : Form
     {
+        #region Variables
+        private int buttonsLoaded = 0;
+        private Button[] posButtons = new Button[100];
+        private Boolean bFirstLoad = true;
+        #endregion
+        #region Constructor
         public FrmSales()
         {
             InitializeComponent();
         }
+        #endregion
 
+        #region LoadGUI
         public void LoadGUI()
         {
+            //Fore Colors
+            btnDelete.ForeColor = Methods.DetermineFrontColor(Methods.clrMenu);
+            btnPay.ForeColor = Methods.DetermineFrontColor(Methods.clrMenu);
+            btnDiscBill.ForeColor = Methods.DetermineFrontColor(Methods.clrMenu);
+            btnDiscItem.ForeColor = Methods.DetermineFrontColor(Methods.clrMenu);
+            //Back Colors
             this.BackColor = Methods.clrForms;
+            pnlBill.BackColor = Methods.ChangeColorBrightness(Methods.clrMenu, 0.05);
+            pnlBillButtons.BackColor = Methods.ChangeColorBrightness(Methods.clrMenu, 0.05);
+            //Misc
+            LoadButtons(100);
+            LocateButtons(buttonsLoaded);
         }
+        #endregion
+
+        #region POS Buttons
+        public void LoadButtons(int ButtonAmnt)
+        {
+            for (int i = 1; i <= ButtonAmnt; i++)
+            {
+                buttonsLoaded++;
+                posButtons[buttonsLoaded-1] = new Button();
+                posButtons[buttonsLoaded-1].Height = 100;
+                posButtons[buttonsLoaded-1].Width = 150;
+                posButtons[buttonsLoaded-1].ForeColor = Methods.DetermineFrontColor(Methods.clrMenu);
+                posButtons[buttonsLoaded-1].Text = "Item " + buttonsLoaded.ToString();
+                posButtons[buttonsLoaded-1].Parent = pnlButtons;
+                pnlButtons.Controls.Add(posButtons[buttonsLoaded-1]);
+                posButtons[buttonsLoaded-1].Name = buttonsLoaded.ToString();
+            }
+        }
+
+        public void LocateButtons(int ButtonAmnt)
+        {
+            pnlButtons.VerticalScroll.Value = 0;
+            pnlButtons.HorizontalScroll.Value = 0;
+            int buttonsLocated = 0;
+            double verticleLines = 1.00 * ButtonAmnt / (pnlButtons.Size.Width / (150 + 6));
+            double horisontalLines = 1.00 * pnlButtons.Size.Width / (150 + 6);
+            for (int y = 1; y <= Math.Ceiling(verticleLines); y++)
+            {
+                for (int x = 1; x <= Math.Floor(horisontalLines); x++)
+                {
+                    if (buttonsLocated < ButtonAmnt)
+                    {
+                        buttonsLocated++;
+                        posButtons[buttonsLocated - 1].Location = new Point((((x - 1) * 150) + (x) * 6), (((y - 1) * 100) + (y) * 6));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            pnlButtons.AutoScroll = true;
+        }
+        #endregion
+        #region Pannel Size Change
+        private void pnlButtons_SizeChanged(object sender, EventArgs e)
+        {
+            if (!bFirstLoad)
+            {
+                LocateButtons(buttonsLoaded);
+            }
+            bFirstLoad = false;
+        }
+        #endregion
     }
 }
