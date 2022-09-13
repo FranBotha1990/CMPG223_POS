d327e947a30edc9d01857e76c3f13cae2dee52d6
Armin Pretorius <arminp1139@gmail.com>
2022-09-13 20:52:15 +0200

    Fixed GitHub Bug

--- pos_group5_cmpg223/POS_Group5_CMPG223/FrmInventory.cs
+++ pos_group5_cmpg223/POS_Group5_CMPG223/FrmInventory.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace POS_Group5_CMPG223
 {
     public partial class FrmInventory : Form
     {
+        SqlConnection con = new SqlConnection(@"connection string copy paste");
+        SqlDataAdapter adapter;
+        DataSet dataset;
+
         public FrmInventory()
         {
             InitializeComponent();
@@ -21,11 +26,103 @@ namespace POS_Group5_CMPG223
         {
             this.BackColor = Methods.clrForms;
             lblDisplay.Text = "";
+            hScrollBar.Value = 0;
+            hScrollBar.Minimum = 0;
+            hScrollBar.Maximum = 1000;
+            hScrollBar.LargeChange = 20;
+            hScrollBar.SmallChange = 1;
+            refresh();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+            string txt = txtSearch.Text;
+            string sql = $"SELECT * FROM PRODUCT WHERE Description LIKE '%" + txt + "%' ";
+            try
+            {
+                con.Open();
+                dataset = new DataSet();
+                adapter = new SqlDataAdapter(sql, con);
 
+                adapter.Fill(dataset, "PRODUCT");
+
+                dataGridView1.DataSource = dataset;
+                dataGridView1.DataMember = "PRODUCT";
+                con.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            FrmAddProduct myForm = new FrmAddProduct();
+            myForm.ShowDialog();
+
+            try
+            {
+                string sql = $"INSERT INTO PRODUCT VALUES('{myForm.Product_ID}','{myForm.Description}',{myForm.Sell_price},{myForm.Quantity_in_stock}) ";
+                con.Open();
+                adapter = new SqlDataAdapter();
+                SqlCommand cmd = new SqlCommand(sql, con);
+                adapter.InsertCommand = cmd;
+                adapter.InsertCommand.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Entry successfully entered.");
+                refresh();
+            }
+
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            try
+            {
+                int priceScroll = hScrollBar.Value;
+                lblDisplay.Text = priceScroll.ToString();
+                con.Open();
+                SqlCommand cmd = new SqlCommand($"SELECT * FROM PRODUCT WHERE Age<=" + priceScroll, con);
+                adapter = new SqlDataAdapter();
+                dataset = new DataSet();
+
+                adapter.SelectCommand = cmd;
+                adapter.Fill(dataset, "PRODUCT");
+
+                dataGridView1.DataSource = dataset;
+                dataGridView1.DataMember = "PRODUCT";
+
+                con.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
+        private void refresh()
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM PRODUCT", con);
+                adapter = new SqlDataAdapter();
+                dataset = new DataSet();
+                adapter.SelectCommand = cmd;
+                adapter.Fill(dataset, "PRODUCT");
+                dataGridView1.DataSource = dataset;
+                dataGridView1.DataMember = "PRODUCT";
+                con.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show("SQL Error Occurred.");
+            }
         }
     }
 }
