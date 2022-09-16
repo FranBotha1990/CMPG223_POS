a738fdd02d437c6d21ed33b823998018ebd8c06f
FranBotha1990 <112196714+FranBotha1990@users.noreply.github.com>
2022-09-16 22:09:30 +0200

    PO delete function added

--- POS_Group5_CMPG223/POS_Group5_CMPG223/FrmPurchaseOrders.cs
+++ POS_Group5_CMPG223/POS_Group5_CMPG223/FrmPurchaseOrders.cs
@@ -98,6 +98,56 @@ namespace POS_Group5_CMPG223
             }
         }
 
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Use the selected row, first cell
+                int selector = (int)dgvPurchaseOrders.CurrentRow.Cells[0].Value;
+
+                //Remove the PO Items first
+                Methods.SQLCon.Open();
+                commandDelete = new SqlCommand($"DELETE FROM PURCHASE_ORDER_ITEM " +
+                                       $"WHERE Purchase_ID LIKE '{selector}'",
+                                       Methods.SQLCon);
+                adapter.DeleteCommand = commandDelete;
+                adapter.DeleteCommand.ExecuteNonQuery();
+                Methods.SQLCon.Close();
+
+                //Clear the listbox and total label
+                lbxItems.Items.Clear();
+                lblTotalAmnt.Text = " ";
+
+                //Remove the PO
+                Methods.SQLCon.Open();
+                commandDelete = new SqlCommand($"DELETE FROM PURCHASE_ORDER " +
+                                       $"WHERE Purchase_ID LIKE '{selector}'",
+                                       Methods.SQLCon);
+                adapter.DeleteCommand = commandDelete;
+                adapter.DeleteCommand.ExecuteNonQuery();
+                
+                //Refresh the data grid
+                Methods.SQLCon.Close();
+                //Join PO and Supplier tables by Supplier_ID
+                command = new SqlCommand(@"SELECT Purchase_ID AS 'PO Number', 
+                                           Supplier_name AS 'Supplier Name', 
+                                           Purchase_date AS 'Purchase Date'
+                                           FROM PURCHASE_ORDER AS PO
+                                           LEFT JOIN SUPPLIER AS SU ON SU.Supplier_ID = PO.Supplier_ID",
+                                           Methods.SQLCon);
+                DataSet dataSet = new DataSet();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet, "PURCHASE_ORDER");
+                dgvPurchaseOrders.DataSource = dataSet;
+                dgvPurchaseOrders.DataMember = "PURCHASE_ORDER";
+                Methods.SQLCon.Close();
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+
         private void btnRemove_Click(object sender, EventArgs e)
         {
             try
@@ -109,7 +159,6 @@ namespace POS_Group5_CMPG223
                 double total = 0;
 
                 Methods.SQLCon.Open();
-
                 SqlDataReader dataReader;
                 //Join the POItems and Product tables by Product_ID
                 command = new SqlCommand($"SELECT POI.Purchase_ID, " +
@@ -122,6 +171,7 @@ namespace POS_Group5_CMPG223
                                            $"WHERE POI.Purchase_ID LIKE '{selector}'",
                                            Methods.SQLCon);
                 dataReader = command.ExecuteReader();
+                //Find the product ID of the selected item
                 while (dataReader.Read())
                 {
                     if (validator.Contains(Convert.ToString(dataReader.GetValue(4))))
@@ -129,9 +179,9 @@ namespace POS_Group5_CMPG223
                         product = Convert.ToInt32(dataReader.GetValue(1));
                     }
                 }
-
                 Methods.SQLCon.Close();
 
+                //Remove the record based on PO ID and Product ID
                 Methods.SQLCon.Open();
                 commandDelete = new SqlCommand($"DELETE FROM PURCHASE_ORDER_ITEM " +
                                        $"WHERE Purchase_ID LIKE '{selector}' " +
@@ -156,19 +206,16 @@ namespace POS_Group5_CMPG223
                                            $"WHERE POI.Purchase_ID LIKE '{selector}'",
                                            Methods.SQLCon);
                 dataReader2 = command.ExecuteReader();
+                //Add all items remaining in the PO
                 while (dataReader2.Read())
                 {
                     string str = string.Format("{0} {1} {2} {3} {4}", dataReader2.GetValue(2), " * ", dataReader2.GetValue(4), " @ R", Convert.ToDouble(dataReader2.GetValue(3)));
                     lbxItems.Items.Add(str);
                     total += (Convert.ToDouble(dataReader2.GetValue(3)) * Convert.ToDouble(dataReader2.GetValue(2)));
                 }
-
-                lblTotalAmnt.Text = string.Format("{0} {1:0.00}", "R", Convert.ToDouble(total));
-
                 Methods.SQLCon.Close();
 
-                MessageBox.Show(validator);
-
+                lblTotalAmnt.Text = string.Format("{0} {1:0.00}", "R", Convert.ToDouble(total));
             }
             catch (SqlException error)
             {
